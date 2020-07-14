5456a452ccfc03fb6bda06a5e52732fd4d267b07
fermartinotti <fermartinotti@gmail.com>
2020-07-14 10:25:39 -0300

    fix build

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/arqsoft-insumos-frontend'));
+app.use(express.static('./dist/arq1-front'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/arqsoft-insumos-frontend/'}),
+    res.sendFile('index.html', {root: 'dist/arq1-front/'}),
 );
 
 app.listen(process.env.PORT || 8080);
