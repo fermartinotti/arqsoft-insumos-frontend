1c87b82c938eafc58bf67574e43bd1e83cd7f2a1
fermartinotti <fermartinotti@gmail.com>
2020-07-13 22:50:27 -0300

    update serverjs

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static('./dist/angular-heroku'));
+app.use(express.static('./dist/arqsoft-insumos-frontend'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/angular-heroku/'}),
+    res.sendFile('index.html', {root: 'dist/arqsoft-insumos-frontend/'}),
 );
 
 app.listen(process.env.PORT || 8080);
